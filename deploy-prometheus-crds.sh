0ed341b39387956edbb51d2584da5b370d39fa9c
Alejandro Galue <agalue@opennms.org>
2023-08-23 08:37:11 -0400

    Update README and ensure everything works with latest Linkerd and Mimir.

--- deploy-prometheus-crds.sh
+++ deploy-prometheus-crds.sh
@@ -5,7 +5,7 @@ trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 type kubectl >/dev/null 2>&1 || { echo >&2 "kubectl required but it's not installed; aborting."; exit 1; }
 
-BASE="https://raw.githubusercontent.com/prometheus-community/helm-charts/main/charts/kube-prometheus-stack/crds/crd-"
+BASE="https://raw.githubusercontent.com/prometheus-community/helm-charts/main/charts/kube-prometheus-stack/charts/crds/crds/crd-"
 
 for YAML in \
   alertmanagerconfigs.yaml \
