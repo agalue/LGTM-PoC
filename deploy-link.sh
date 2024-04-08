bdc6b13aa7f069a87d87f481fd2f3d598e43f4d5
Alejandro Galue <galue.alejandro@gmail.com>
2024-04-08 18:18:58 -0400

    Adding copies of the shared services into remote clusters.

--- deploy-link.sh
+++ deploy-link.sh
@@ -17,6 +17,16 @@ REMOTE_CTX=kind-${CONTEXT}
 echo "Creating link from ${REMOTE_CTX} to ${CENTRAL_CTX}"
 if [[ "${CILIUM_CLUSTER_MESH_ENABLED}" == "yes" ]]; then
   cilium clustermesh connect --context ${REMOTE_CTX} --destination-context ${CENTRAL_CTX}
+
+  # Create copies of the global services
+  kubectl --context ${CENTRAL_CTX} get service/mimir-distributor -n mimir -o yaml | \
+    sed '/clusterIP:/,+2d' | kubectl --context ${REMOTE_CTX} apply -f -
+  kubectl --context ${CENTRAL_CTX} get service/tempo-distributor -n tempo -o yaml | \
+    sed '/clusterIP:/,+2d' | kubectl --context ${REMOTE_CTX} apply -f -
+  kubectl --context ${CENTRAL_CTX} get service/loki-write -n loki -o yaml | \
+    sed '/clusterIP:/,+2d' | kubectl --context ${REMOTE_CTX} apply -f -
+  kubectl --context ${CENTRAL_CTX} get service/monitor-alertmanager -n observability -o yaml | \
+    sed '/clusterIP:/,+2d' | kubectl --context ${REMOTE_CTX} apply -f -
 else
   # The following is required when using Kind/Docker without apiServerAddress on Kind config.
   API_SERVER=$(kubectl get node --context ${CENTRAL_CTX} -l node-role.kubernetes.io/control-plane -o json \
