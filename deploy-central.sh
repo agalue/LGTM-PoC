f7cba115becc9298d300fd9428b082d4a83e5cb0
Alejandro Galue <galue.alejandro@gmail.com>
2026-01-02 14:25:12 -0500

    Full OTEL Collector Deployment (no Prometheus). (#63)

--- deploy-central.sh
+++ deploy-central.sh
@@ -26,6 +26,7 @@ helm repo add linkerd https://helm.linkerd.io/stable
 helm repo add linkerd-edge https://helm.linkerd.io/edge
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add vector https://helm.vector.dev
 helm repo add fluent https://fluent.github.io/helm-charts
